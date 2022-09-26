c10098c33acebc2328855bdb8584dfe827a75aac
Ilya Gulenkin <gulenkini@yandex.ru>
2022-09-26 11:17:31 +0700

    create pagination on page and add propTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Users from "./components/users";
 import SearchStatus from "./components/searchStatus";
-import api from "./api"
+import api from "./api";
 
 function App() {
   const [users, setUsers] = useState(api.users.fetchAll());
@@ -10,21 +10,26 @@ function App() {
     setUsers(users.filter((user) => user._id !== userId));
 
   const handleToggleBookMark = (id) => {
-    setUsers(users.map(user => {
-      if (user._id === id) {
-        return { ...user, bookmark: !user.bookmark }
-      }
-      return user
-    })
+    setUsers(
+      users.map((user) => {
+        if (user._id === id) {
+          return { ...user, bookmark: !user.bookmark };
+        }
+        return user;
+      })
     );
-  }
+  };
 
   return (
     <div>
       <SearchStatus length={users.length} />
-      <Users users={users} onDelete={handleDelete} onToggleBookMark={handleToggleBookMark} />
+      <Users
+        users={users}
+        onDelete={handleDelete}
+        onToggleBookMark={handleToggleBookMark}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
